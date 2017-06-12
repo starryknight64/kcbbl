6aabd0355c36336ab3f28714cd558ee8bee03805
Phillip <pdponzer@gmail.com>
2017-06-11 19:12:49 -0500

    Got the boilerplate into a reasonable state

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require("./routes/index");
+var coaches = require("./routes/coaches");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+//app.use(favicon(__dirname + "/public/favicon.ico"));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use("/js", express.static(__dirname + "/node_modules/jquery/dist"))
+app.use("/js", express.static(__dirname + "/node_modules/bootstrap/dist/js"))
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"))
+app.use("/css", express.static(__dirname + "/css"))
+app.use("/images", express.static(__dirname + "/images"))
+
+app.use("/", routes);
+app.use("/coaches", coaches);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -36,10 +42,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
       message: err.message,
       error: err
     });
@@ -50,7 +56,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error', {
+  res.render("error", {
     message: err.message,
     error: {}
   });
