f3b52683895cc39c33aa664433b823aac4f0c04b
Phillip <pdponzer@gmail.com>
2017-06-15 22:25:33 -0500

    Laid out REST routes and added body-container CSS

---
+++ routes/seasons.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+var seasonsREST = require("./rest/seasons")
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("seasons", { seasons: [{ id: 1, name: "Season 1" }, { id: 2, name: "Season 2" }] });
+});
+
+module.exports = router;
